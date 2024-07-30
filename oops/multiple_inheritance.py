ab2f44fa8d32101a4247c7b98b9728709ca5677c
rohit kumar <12rohit4@gmail.com>
2024-07-30 22:38:28 +0530

    adding comment for mro

--- oops/multiple_inheritance.py
+++ oops/multiple_inheritance.py
@@ -24,6 +24,23 @@ class C(A,B):
         a=12
         print(a)
 
+class D(B,A):
+    def __init__(self):
+        pass
+    def call(self):
+        super().check()
+    def check(self):
+        a=13
+        print(a)
+
 obj_c = C()
 obj_c.check()
 obj_c.call()
+
+obj_d = D()
+obj_d.check()
+obj_d.call()
+# check the method order for a particular class 
+# python uses C3 linearization algorithm to evaluate the order
+# sample output (<class '__main__.D'>, <class '__main__.B'>, <class '__main__.A'>, <class 'object'>)
+print(D.__mro__)

284071dc2be9518284ef0ad6998536c4e4f2fcab
rohit kumar <12rohit4@gmail.com>
2024-07-31 00:32:16 +0530

    adding a question in comments to be solved

---
+++ oops/circle.py
@@ -0,0 +1,7 @@
+# The circle should have a radius, a diameter, and an area. It should also have a nice string representation
+#  make sure when the radius of your class changes that the diameter and area both change as well:
+# make sure you can set the diameter attribute in your class and the radius will update accordingly.
+#  Also make sure also that you cannot set the area (setting area should raise an AttributeError):
+# make sure your radius cannot be set to a negative number.
+# You should raise a ValueError exception with the error message 
+# Radius cannot be negative (yes the tests check for the error message).

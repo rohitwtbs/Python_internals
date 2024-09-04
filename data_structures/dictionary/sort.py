73cfcfa44dae3b9f1e803c4b847dc9b9b9e8b818
rohitwtbs <12rohit4@gmail.com>
2024-09-04 19:03:14 +0530

    bracket correction

--- data_structures/dictionary/sort.py
+++ data_structures/dictionary/sort.py
@@ -3,6 +3,6 @@
 d = {"Bangalore":25, "leh":-2,"Delhi":45,"Chennai":43}
 print(d)
 print(d.items())
-print(sorted(d.items())
+print(sorted(d.items()))
 sorted_dict = dict(sorted(d.items(), key=lambda item: item[1], reverse=False))
 print(sorted_dict)

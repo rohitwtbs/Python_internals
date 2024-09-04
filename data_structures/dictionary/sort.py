8309d2c1d314f080118c4fa492466d72abbc70aa
rohitwtbs <12rohit4@gmail.com>
2024-09-04 19:02:33 +0530

    more gists

---
+++ data_structures/dictionary/sort.py
@@ -0,0 +1,8 @@
+# write a program to sort a dictonary according to its values
+
+d = {"Bangalore":25, "leh":-2,"Delhi":45,"Chennai":43}
+print(d)
+print(d.items())
+print(sorted(d.items())
+sorted_dict = dict(sorted(d.items(), key=lambda item: item[1], reverse=False))
+print(sorted_dict)

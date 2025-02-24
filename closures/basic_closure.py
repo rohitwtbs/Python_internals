fe3f9acd278c58ce1bfa772131a48296561af392
rohit kumar <12rohit4@gmail.com>
2025-02-24 19:37:54 +0530

    the iiner fucntion remembers the parameter passed to it

--- closures/basic_closure.py
+++ closures/basic_closure.py
@@ -1,9 +1,9 @@
-def outer_fucntion():
+def outer_fucntion(msg):
     def inner_function():
-        print("inner fucntion executed")
+        print("inner fucntion executed",msg)
     return inner_function
 
-closure = outer_fucntion()
+closure = outer_fucntion("new msg")
 # closure = outer_fucntion
 # execute the closure now
 print(closure)

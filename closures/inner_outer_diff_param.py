ace8ded6269e794fb86526fa61791a8d043732ac
rohit kumar <12rohit4@gmail.com>
2025-02-24 19:46:22 +0530

    adding example with diff params from outer adn inner fucntion

---
+++ closures/inner_outer_diff_param.py
@@ -0,0 +1,8 @@
+def outer_fucntion(msg_outer):
+    def inner_function(msg_inner):
+        print("log from inner fucntion", msg_inner + " " + msg_outer)
+    return inner_function
+
+closure = outer_fucntion("rohit")
+
+closure("kumar")

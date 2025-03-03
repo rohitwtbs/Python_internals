13ea165f6dfc83f472485e8f964b3c18905e68f8
rohit kumar <12rohit4@gmail.com>
2025-03-03 14:57:46 +0530

    passing container types such as list to a lambda

--- lambda/simple.py
+++ lambda/simple.py
@@ -4,3 +4,10 @@ x = 50
 y = lambda x: x*x+2
 print(y) # lambdas donot consider the scope of the variable
 print(y(x))
+
+# passing container types to a lambda
+
+ls =[2,3,4,5,6]
+
+z = lambda x:sum(x)
+print(z(ls))

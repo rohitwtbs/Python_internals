2154170df1378dad0b114ccfa31c2abeb949fa2c
rohit kumar <12rohit4@gmail.com>
2025-03-04 02:10:04 +0530

    going to finish deququw first thing in the morning

---
+++ collections/deque.py
@@ -0,0 +1,17 @@
+class Node():
+    def __init__(self,data):
+        self.next = None
+        self.previous = None
+        self.data = data
+
+class Deque():
+    def __init__(self):
+        pass
+    def pop(self):
+        pass
+    def pop_left():
+        pass
+    def append():
+        pass
+    def append_left():
+        pass

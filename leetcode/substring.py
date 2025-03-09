d60ea607218c9a0f8da1efecdb4bad986bc8bada
rohitwtbs <12rohit4@gmail.com>
2025-03-09 16:22:14 +0530

    substring example

---
+++ leetcode/substring.py
@@ -0,0 +1,11 @@
+def get_all_substrings(input_string):
+    substrings = []
+    for i in range(len(input_string)):
+        for j in range(i + 1, len(input_string) + 1):
+            substrings.append(input_string[i:j])
+    return substrings
+
+
+my_string = "rohitkumar"
+all_substrings = get_all_substrings(my_string)
+print(all_substrings)

750b18df6391bd6d2e80d1efc8ea09464e81cb88
rohitwtbs <12rohit4@gmail.com>
2025-03-10 00:35:25 +0530

    add modified code

--- leetcode/longest_substring_no_repeat.py
+++ leetcode/longest_substring_no_repeat.py
@@ -1,27 +1,25 @@
-# Longest Substring Without Repeating Characters
-
-
 class Solution:
+    def get_longest_string(self,s):
+        for i in s:
+            if(self.norepeat(i) == True):
+                return len(i)
+        return 0
+
+    def norepeat(self,s):
+        frequency = {}
+        for i in s:
+            if(frequency.get(i,0) >= 1):
+                return False
+            else:
+                frequency[i] = frequency.get(i,0) + 1
+        return True
+
     def lengthOfLongestSubstring(self, s: str) -> int:
         substrings = []
         for i in range(len(s)):
             for j in range(i+1,len(s)+1):
                 substrings.append(s[i:j])
         substrings.sort(key=len,reverse=True)
-        return get_longest_string(substrings)
-
-    def get_longest_string(s):
-        for i in s:
-            if(norepeat(i) == True):
-                return len(i)
-    
-    def norepeat(s):
-        frequency = {}
-        for i in s:
-            if(frequency.get(i,0) => 1):
-                return False
-            else:
-                frequency[i] = frequenct.get(i,0) + 1
-        return True
+        return self.get_longest_string(substrings)
 
 

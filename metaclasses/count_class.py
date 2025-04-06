22aedb1d03bf346d153e3ce18061e5dac100c9a3
rohitwtbs <12rohit4@gmail.com>
2025-04-07 02:03:51 +0530

    with and without meta class

--- metaclasses/count_class.py
+++ metaclasses/count_class.py
@@ -1,6 +1,7 @@
 class CountMeta(type):
     count = 0
     def __new__(cls, name, bases, dct):
+        print(f"Creating class {name} with bases {bases} and dct {dct}")
         cls.count += 1
         return super().__new__(cls, name, bases, dct)
 
@@ -9,3 +10,16 @@ class B(A): pass
 class C(B): pass
 
 print(CountMeta.count)
+
+
+# what happens if this is a normal class ?
+
+
+class CountClass:
+    count = 0
+    def __init__(self):
+        CountClass.count += 1
+class A(CountClass): pass
+class B(A): pass
+class C(B): pass
+print(CountClass.count)

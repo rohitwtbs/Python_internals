2d6372898a541cdc552ff57a8cb876b77aff2de9
rohitwtbs <12rohit4@gmail.com>
2025-04-07 02:10:21 +0530

    who is daddy

---
+++ metaclasses/who_is_daddy.py
@@ -0,0 +1,9 @@
+class Meta(type):
+    def __new__(cls, name, bases, dct):
+        print(f"Creating class {name}")
+        return super().__new__(cls, name, bases, dct)
+
+class MyClass(metaclass=Meta):
+    pass
+
+obj = MyClass()

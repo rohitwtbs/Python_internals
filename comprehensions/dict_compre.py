31a864adccc44d407d6b4ef58a7f4a65d56dadb0
rohitwtbs <12rohit4@gmail.com>
2025-04-20 02:06:55 +0530

    explain this behaviour

--- comprehensions/dict_compre.py
+++ comprehensions/dict_compre.py
@@ -1,4 +1,6 @@
 sample = {x: x*x for x in range(10)}
 sampl2 = {y:x*x for x in range(10) for y in range(10)}
+smaple3 = {x: x*y*z for x in range(10) for y in range(10) for z in range(10)}
 print(sample)
 print(sampl2)
+print(smaple3)

ebd1e2dd53173744cb07f6a7ab7504f551717ecc
rohitwtbs <12rohit4@gmail.com>
2025-04-27 19:42:14 +0530

    add comment to understand working later

--- decorator/func_decorator.py
+++ decorator/func_decorator.py
@@ -10,3 +10,10 @@ def add(x,y):
 
 
 add(1,2)
+
+# Notes
+# when no return statement is present in the decorator function, it throws an error
+# Traceback (most recent call last):
+#   File "/Users/rohitwtbs/Documents/github/Python_internals/decorator/func_decorator.py", line 12, in <module>
+#     add(1,2)
+# TypeError: 'NoneType' object is not callable

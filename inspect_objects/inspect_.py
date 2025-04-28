edd4f19063e892750a13b4b42f992ac487473857
rohitwtbs <12rohit4@gmail.com>
2025-04-28 08:18:32 +0530

    got the source code logged with classname

--- inspect_objects/inspect_.py
+++ inspect_objects/inspect_.py
@@ -10,7 +10,9 @@ class Prime():
 
 
 def check_objects(obj):
-    code = inspect.getsource(obj)
+    #  this is not working as an instance is passed
+    # code = inspect.getsource(obj)
+    code = inspect.getsource(obj.__class__)
     print("Source code:\n", code)
     pass
 

fda8c1672231b540f90fbec6a3a07682da00a2df
rohitwtbs <12rohit4@gmail.com>
2025-05-06 18:44:04 +0530

    correction

--- data_structures/dictionary/get_dict_elements.py
+++ data_structures/dictionary/get_dict_elements.py
@@ -1,9 +1,9 @@
 
-A = {min: 2}
+A = {"min": 2}
 
-B = {min: 2, max: 3}
+B = {"min": 2, "max": 3}
 
-C = {min: 2, max: 3, sum: 5}
+C = {"min": 2, "max": 3, "sum": 5}
 
 
 # print(A["min"] + B["min"] + C["min"])

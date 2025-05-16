54b7cf8fe97b50d799823c0706bbd93500695351
rohitwtbs <12rohit4@gmail.com>
2025-05-16 19:10:03 +0530

    adding more constants

--- arcade/pymunk_platformer.py
+++ arcade/pymunk_platformer.py
@@ -1,8 +1,24 @@
 import arcade
 
-screen_title = "Pymunk Platformer"
-screen_width = 800
-screen_height = 600
+SCREEN_TITLE = "PyMunk Platformer"
+
+# How big are our image tiles?
+SPRITE_IMAGE_SIZE = 128
+
+# Scale sprites up or down
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_TILES = 0.5
+
+# Scaled sprite size for tiles
+SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING_PLAYER)
+
+# Size of grid to show on screen, in number of tiles
+SCREEN_GRID_WIDTH = 25
+SCREEN_GRID_HEIGHT = 15
+
+# Size of screen to show, in pixels
+SCREEN_WIDTH = SPRITE_SIZE * SCREEN_GRID_WIDTH
+SCREEN_HEIGHT = SPRITE_SIZE * SCREEN_GRID_HEIGHT
 
 class GameWindow(arcade.Window):
 
@@ -26,7 +42,7 @@ class GameWindow(arcade.Window):
 
 
 def main():
-    window = GameWindow(screen_width, screen_height, screen_title)
+    window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
     arcade.run()
 

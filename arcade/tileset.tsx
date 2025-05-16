9b857c3f712d0d32e14b5ad886cbd1abbb330c24
rohitwtbs <12rohit4@gmail.com>
2025-05-16 20:13:59 +0530

    got the tmx fiel from chatgpt

---
+++ arcade/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.10.2" name="tileset" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="tile.png" width="64" height="64"/>
+</tileset>

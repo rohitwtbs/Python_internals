4c58c9a0ed9d3459632c03fa087012ecd75d67fe
rohit kumar <12rohit4@gmail.com>
2025-07-29 00:14:13 +0530

    why is gc.collect output 0

--- garbage_collector/reference_counting.py
+++ garbage_collector/reference_counting.py
@@ -10,3 +10,23 @@ print(sys.getrefcount(a))
 del b
 print(sys.getrefcount(a))
 del a
+
+# lets see for a cyclic referrence
+# gc.collect()  # Uncomment to force garbage collection
+
+
+class Node():
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+    def add_adress(self, node):
+        self.next = node
+
+
+node1 = Node(1)
+node2 = Node(2)
+node1.add_adress(node2)
+node2.add_adress(node1)
+
+print(gc.collect())  # This will collect the cyclic references

45cbb7f0dabf8b4e013211b1961da33f04457807
rohit kumar <12rohit4@gmail.com>
2025-07-29 00:28:25 +0530

    why line 35 prints 0

--- garbage_collector/reference_counting.py
+++ garbage_collector/reference_counting.py
@@ -29,4 +29,30 @@ node2 = Node(2)
 node1.add_adress(node2)
 node2.add_adress(node1)
 
-print(gc.collect())  # This will collect the cyclic references
+references = gc.collect()
+
+
+print(references)  # Why this prints 0 
+
+
+
+
+# Reference cycle example
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.ref = None
+
+# Create a cycle
+node1 = Node(1)
+node2 = Node(2)
+node1.ref = node2
+node2.ref = node1  # Cycle created!
+
+# Even after deleting variables, objects might stay in memory
+# due to the cycle, until garbage collector runs
+del node1, node2
+
+# Force garbage collection
+collected = gc.collect()
+print(f"Collected {collected} objects")

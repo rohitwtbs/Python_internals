93fb637237c2030544c0e911ce9b65301e3d11ad
rohit kumar <12rohit4@gmail.com>
2025-12-30 14:00:00 +0000

    "checking if heapq reduces time complexity"

--- leetcode/merge_array.py
+++ leetcode/merge_array.py
@@ -1,7 +1,7 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        j = nums1 + nums2
-        j.sort()
+        j =  list(heapq.merge(nums1, nums2))
+        # j.sort()
         if (len(j) % 2 == 0):
             index = len(j) // 2 
             # return (j[index] + j[index - 1])

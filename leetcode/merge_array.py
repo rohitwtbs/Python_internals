2b814b2e2e7952fef9a3870794fd112b7d05fd3a
rohit kumar <12rohit4@gmail.com>
2025-12-30 13:48:13 +0000

    "leetcode"

---
+++ leetcode/merge_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        j = nums1 + nums2
+        j.sort()
+        if (len(j) % 2 == 0):
+            index = len(j) // 2 
+            # return (j[index] + j[index - 1])
+            return (j[index] + j[index -1 ]) / 2
+            pass
+        else:
+            return  j[(len(j) // 2)]
+
+        # print(j)
+
+        return 0
